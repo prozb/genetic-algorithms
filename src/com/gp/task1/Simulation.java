c6cf2352ef75157be37010bb4fd17f8a7bc68e23
= <rozbich15@gmail.com>
2018-11-12 01:18:40 +0100

    finished user input

--- src/com/gp/task1/Simulation.java
+++ src/com/gp/task1/Simulation.java
@@ -30,45 +30,47 @@ public class Simulation {
     }
 
     private static void processUserInput(String [] args){
-//        if(args.length <= 1){
-//            printHelpMessage();
-//            printError("No parameters!");
-//        }else if(args[1].equals("--help")){
-//            printHelpMessage();
-//            System.exit(99);
-//        }else if(args.length < NUM_OF_ARGS){
-//            printHelpMessage();
-//            printError("Not enough parameters!");
-//        }else if(args.length > NUM_OF_ARGS){
-//            printHelpMessage();
-//            printError("To many parameters!");
-//        }else {
-            generationCount   = getIntegerValue("--genecount");
-            geneLen           = getIntegerValue("--genelen");
-            crossOverSchema   = getIntegerValue("--crossover_scheme");
-            replicationSchema = getIntegerValue("--replication_scheme");
-            initRate          = getIntegerValue("--initrate");
-
-//        }
+        if(args.length <= 1){
+            printHelpMessage();
+            printError("No parameters!");
+        }else if(args[1].equals("--help")){
+            printHelpMessage();
+            System.exit(99);
+        }else if(args.length < NUM_OF_ARGS){
+            printHelpMessage();
+            printError("Not enough parameters!");
+        }else if(args.length > NUM_OF_ARGS){
+            printHelpMessage();
+            printError("To many parameters!");
+        }else {
+            generationCount   = (int) getValue("--genecount");
+            geneLen           = (int) getValue("--genelen");
+            crossOverSchema   = (int) getValue("--crossover_scheme");
+            replicationSchema = (int) getValue("--replication_scheme");
+            initRate          = (int) getValue("--initrate");
+            maxGenerations    = (int) getValue("--maxgen");
+            mutationRate      = getValue("--pm");
+            recombinationRate = getValue("pc");
+        }
     }
 
-    private static int getIntegerValue(String arg){
+    private static float getValue(String arg){
         Optional<String> geneLenStringOptional = Arrays.stream(arguments).filter(elem -> elem.contains(arg)).findFirst();
         String geneLenString = "";
 
         if(geneLenStringOptional.isPresent()){
-            geneLenString = geneLenStringOptional.get();
+            geneLenString = geneLenStringOptional.get().trim();
         }
         int pos = geneLenString.indexOf(arg) + 1 + arg.length();
         String num = geneLenString.substring(pos);
 
-        int geneLen = 0;
+        float val = 0;
         try {
-            geneLen = Integer.parseInt(num);
+            val = Float.parseFloat(num);
         }catch (NumberFormatException e){
-            printError("Integer \"" + arg + "\" not found!");
+            printError("Number \"" + arg + "\" not found!");
         }
-        return geneLen;
+        return val;
     }
 
     private static void printError(String err){
@@ -85,21 +87,15 @@ public class Simulation {
                 "[--genecnt] count of genes in generation (integer number)\n" +
                 "[--genelen] length of the gene (integer number)\n" +
                 "[--crossover_scheme] (integer number)\n" +
-                "[--replication_scheme] (integer number)");
+                "[--replication_scheme] (integer number)\n" +
+                "[--maxgen] maximum generations (integer number)");
     }
 
     private static void startSimulation() throws InterruptedException {
-        int generationLen  = 200;
-        int geneLen        = 200;
-        int initRate       = 5;
-        float mutationRate = 0.02f;
-        int replicationSch = 1;
-        int crossOverSch   = 1;
-        float recombRate   = 0.5f;
 
-        int runsCount = 2000;
+        int runsCount = maxGenerations;
 
-        DNAPool pool = new DNAPool(generationLen, geneLen, initRate, mutationRate, replicationSch, crossOverSch, recombRate);
+        DNAPool pool = new DNAPool(generationCount, geneLen, initRate, mutationRate, replicationSchema, crossOverSchema, recombinationRate);
         while(!pool.isFinished() && runsCount > 0){
             pool.processCrossOver();
             pool.sortGeneration();
