6c561a16aea562885670dd942183a7bfcb9c9bad
= <rozbich15@gmail.com>
2018-11-25 18:09:22 +0100

    completely implemented threaded version

--- src/com/gp/task1/Main.java
+++ src/com/gp/task1/Main.java
@@ -50,7 +50,7 @@ public class Main {
 
         int permNumber          =  (int)((((Constants.PM_MAX - Constants.PM_MIN) / Constants.PM_STEP) *
                                 ((Constants.PC_MAX - Constants.PC_MIN) / Constants.PC_STEP)) + 0.5f);
-        int threadsNum          = 8;
+        int threadsNum          = 1;
         //creates array with points with pm and pc to pass into each simulation
         calculatePointsToProcess(permNumber);
 
