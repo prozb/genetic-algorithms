4891e7bf097654d60307ab83f891aa3e281327c5
= <rozbich15@gmail.com>
2018-11-26 08:43:29 +0100

    added gnuplot scripts

--- src/com/gp/task1/Constants.java
+++ src/com/gp/task1/Constants.java
@@ -9,17 +9,18 @@ package com.gp.task1;
 public class Constants {
     public final static int S            = 5;      //ranking constant
     public final static float PC_MIN     = 0.5f;
-    public final static float PC_MAX     = 0.54f;
-//    public final static float PC_MAX     = 0.9f;
+//    public final static float PC_MAX     = 0.54f;
+    public final static float PC_MAX     = 0.9f;
     public final static float PC_STEP    = 0.02f;
-    public final static float PM_MIN     = 0.002f;
-    public final static float PM_MAX     = 0.008f;
+    public final static float PM_MIN     = 0.000f;
+//    public final static float PM_MAX     = 0.008f;
 //    public final static float PM_MAX     = 0.03f;
+    public final static float PM_MAX     = 0.002f;
     public final static float PM_STEP    = 0.002f;
     public final static int SCALE_FACTOR = 1000000;
     public static final int NUM_OF_ARGS  = 10;
     public static final int GENES_SCALE  = 40;
-    public static final int THREADS_NUM  = 4;      //how much threads will be execute calculation (best way is when
+    public static final int THREADS_NUM  = 6;      //how much threads will be execute calculation (best way is when
                                                    //threads num equals count of physical CPUs)
 
     public static final String DATE_PATTERN      = "dd.MM.yyyy hh:mm:ss";
